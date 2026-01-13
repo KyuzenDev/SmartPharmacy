8e0fd970380ab125b732c498105b15420e82ef63
Kyuzen <antcraft517@gmail.com>
2026-01-13 12:43:18 +0700

    feat(auth): implement login system with JWT and bcrypt validation

--- services/AuthService.ts
+++ services/AuthService.ts
@@ -1,6 +1,7 @@
 // src/services/AuthService.ts
 import { db } from "@/lib/db";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 import { ResultSetHeader, RowDataPacket } from "mysql2";
 
 interface RegisterInput {
@@ -8,6 +9,11 @@ interface RegisterInput {
   email: string;
   password: string;
     role: "PASIEN" | "APOTEKER";
+}
+
+interface LoginInput {
+  email: string;
+  password: string;
 }
 
 export class AuthService {
@@ -45,4 +51,37 @@ export class AuthService {
     );
     return rows;
   }
+<<<<<<< Updated upstream
+=======
+
+  static async login(data: LoginInput) {
+    const { email, password } = data;
+
+    const [rows] = await db.execute<RowDataPacket[]>(
+      "SELECT * FROM User WHERE email = ?",
+      [email]
+    );
+
+    const user = rows[0];
+    if (!user) throw new Error("Email atau password salah");
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) throw new Error("Email atau password salah");
+
+    const token = jwt.sign(
+      { id: user.id, email: user.email, role: user.role },
+      process.env.JWT_SECRET as string,
+      { expiresIn: "1d" }
+    );
+
+    return {
+      token,
+      user: {
+        id: user.id,
+        nama: user.nama,
+        role: user.role,
+      },
+    };
+  }
+>>>>>>> Stashed changes
 }
